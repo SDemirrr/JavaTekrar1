77e84d066f142a63daf9cdecd9124fcf6f71a183
SDemirrr <101672380+SDemirrr@users.noreply.github.com>
2023-12-22 21:17:41 +0300

    22-12-23

---
+++ src/day02_variables/C01_Variables.java
@@ -0,0 +1,25 @@
+package day02_variables;
+
+public class C01_Variables {
+
+    public static void main(String[] args) {
+           int              not               =                        60;
+        //data turu     variable ismi     assignment sign    variable degeri
+
+
+        int not2      =     70;
+        //varaible         deger
+        //java once degeri hesaplar sonra assign islemini yapar
+
+        //not2 yi 90 yapalim
+        not2=90;
+
+
+        //yeni bir int olusturalim degeri ilk iki variblenin ortalamasi olsun
+
+        int ortalama=(not+not2)/2;
+        System.out.println("iki notun ortalamasi : "+ortalama);
+
+
+    }
+}
